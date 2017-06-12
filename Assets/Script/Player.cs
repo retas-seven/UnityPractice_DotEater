38c21b3db6cb40ff925421300de36b5ff3c41a7d
t-osawa <retas_seven@hotmail.co.jp>
2017-06-12 23:03:02 +0900

    first commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 360f;
+
+    CharacterController characterController;
+
+    // Use this for initialization
+    void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (direction.sqrMagnitude > 0.01f)
+        {
+            Vector3 forward = Vector3.Slerp(
+                transform.forward,
+                direction,
+                rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction)
+            );
+            transform.LookAt(transform.position + forward);
+        }
+        characterController.Move(direction * moveSpeed * Time.deltaTime);
+	}
+}
